dc22596cd3b6e06ba65bcc7ce851e8cd102bc4f2
nickdowski <nickdowski@gmail.com>
2025-03-23 13:03:08 -0500

    Add a hover event listener to change background color of squares to black

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ const squares = document.getElementById('square-grid');
 for (let i = 0; i < 256; i++) {
     const square = document.createElement ('div');
     square.classList.add('square');
+    
+    //Change color of square on hover
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'black';
+    })
+    
     squares.appendChild(square);
 }
+
+
+
